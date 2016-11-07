546896cb3c2fedaa53d7a4441e237c1e9954f0a3
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-07 23:56:37 +0100

    uruchamianie operacji na plikach w osobnych watkach

--- src/main/java/pl/ppkwu/lab/impl/FileOperationsImpl.java
+++ src/main/java/pl/ppkwu/lab/impl/FileOperationsImpl.java
@@ -11,56 +11,64 @@ public class FileOperationsImpl implements FileOperations {
     public static final int DELAY_SECS = 3;
 
     public void readFile(String fileName, ReadCallback callback) {
-        BufferedReader reader = null;
-        try {
-            delay(DELAY_SECS);
+        Runnable runnable = () -> {
+            BufferedReader reader = null;
+            try {
+                delay(DELAY_SECS);
 
-            reader = new BufferedReader(new FileReader(new File(fileName)));
-            StringBuilder sb = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-            }
+                reader = new BufferedReader(new FileReader(new File(fileName)));
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                }
 
-            callback.onSuccess(sb.toString());
-        } catch (FileNotFoundException e) {
-            callback.onFailure("File was not found");
-        } catch (IOException e) {
-            callback.onFailure("Unexpected error while reading file");
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                callback.onSuccess(sb.toString());
+            } catch (FileNotFoundException e) {
+                callback.onFailure("File was not found");
+            } catch (IOException e) {
+                callback.onFailure("Unexpected error while reading file");
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-        }
+        };
+
+        new Thread(runnable).start();
     }
 
-    public void writeFile(String fileName, String content, WriteCallback callback) {
-        FileWriter writer = null;
-        try {
-            delay(DELAY_SECS);
+    public void writeFile(final String fileName, final String content, final WriteCallback callback) {
+        Runnable runnable = () -> {
+            FileWriter writer = null;
+            try {
+                delay(DELAY_SECS);
 
-            writer = new FileWriter(new File(fileName));
-            writer.write(content);
+                writer = new FileWriter(new File(fileName));
+                writer.write(content);
 
-            callback.onSuccess();
-        } catch (FileNotFoundException e) {
-            callback.onFailure("Error while creating file");
-        } catch (IOException e) {
-            callback.onFailure("Unexpected error while writing to file");
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                callback.onSuccess();
+            } catch (FileNotFoundException e) {
+                callback.onFailure("Error while creating file");
+            } catch (IOException e) {
+                callback.onFailure("Unexpected error while writing to file");
+            } finally {
+                if (writer != null) {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-        }
+        };
+
+        new Thread(runnable).start();
     }
 
     private void delay(long sec) {
