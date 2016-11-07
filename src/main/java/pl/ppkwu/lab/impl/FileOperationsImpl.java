03f37a0d5f5b1f909953151d83ffcddef8abfec5
Maciej Sulej <187809@edu.p.lodz.pl>
2016-11-07 23:37:30 +0100

    implementacja writeFile

--- src/main/java/pl/ppkwu/lab/impl/FileOperationsImpl.java
+++ src/main/java/pl/ppkwu/lab/impl/FileOperationsImpl.java
@@ -11,9 +11,11 @@ public class FileOperationsImpl implements FileOperations {
     public static final int DELAY_SECS = 3;
 
     public void readFile(String fileName, ReadCallback callback) {
+        BufferedReader reader = null;
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
+            delay(DELAY_SECS);
 
+            reader = new BufferedReader(new FileReader(new File(fileName)));
             StringBuilder sb = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
@@ -21,18 +23,44 @@ public class FileOperationsImpl implements FileOperations {
                 sb.append(System.lineSeparator());
             }
 
-            delay(DELAY_SECS);
-
             callback.onSuccess(sb.toString());
         } catch (FileNotFoundException e) {
             callback.onFailure("File was not found");
         } catch (IOException e) {
             callback.onFailure("Unexpected error while reading file");
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public void writeFile(String fileName, String content, WriteCallback callback) {
+        FileWriter writer = null;
+        try {
+            delay(DELAY_SECS);
 
+            writer = new FileWriter(new File(fileName));
+            writer.write(content);
+
+            callback.onSuccess();
+        } catch (FileNotFoundException e) {
+            callback.onFailure("Error while creating file");
+        } catch (IOException e) {
+            callback.onFailure("Unexpected error while writing to file");
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private void delay(long sec) {
